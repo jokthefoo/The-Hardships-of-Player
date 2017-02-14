28cc4518dbdfe422acd290d0046dce10c9615b24
jokthefoo <alexkofford@gmail.com>
2017-02-13 18:06:46 -0700

    Added blue platforms fading out, and second bad event.

--- HoP/Assets/Scripts/BadEvent.cs
+++ HoP/Assets/Scripts/BadEvent.cs
@@ -6,16 +6,28 @@ using UnityEngine;
 public class BadEvent : MonoBehaviour {
 
     public GameObject target;
+    public string badEventText = "You smell really bad today.\n So yellow has been avoiding you.";
+
     bool triggered;
+    bool showText;
+    float textTimer = 3.0f;
 
 	// Use this for initialization
 	void Start () {
         triggered = false;
+        showText = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(triggered && showText)
+        {
+            textTimer -= Time.deltaTime;
+            if(textTimer < 0)
+            {
+                showText = false;
+            }
+        }
 	}
 
     private void OnTriggerEnter2D(Collider2D coll)
@@ -30,14 +42,43 @@ public class BadEvent : MonoBehaviour {
     void MomBadEvent()
     {
         triggered = true;
-        foreach(Transform t in target.GetComponentsInChildren<Transform>())
+        showText = true;
+        foreach (Transform t in target.GetComponentsInChildren<Transform>())
         {
             if(t.name != target.name)
             {
                 t.localScale = new Vector3(t.localScale.x / 2, t.localScale.y, t.localScale.z);
             }
         }
-        EditorUtility.DisplayDialog("Bad Stuff","You got bronze 5 in league","Ok");
         GetComponentInParent<ParticleSystem>().Stop();
+    }
+
+    void OnGUI()
+    {
+        if (triggered && showText)
+        {
+            GUIStyle style = new GUIStyle();
+            style.fontSize = 35;
+            style.alignment = TextAnchor.MiddleCenter;
+            style.wordWrap = true;
+            DrawOutline(new Rect(Screen.width/2-150, Screen.height/2-25, 450, 50), badEventText, style, Color.black, Color.red, 3);
+        }
+    }
+
+    //draw text of a specified color, with a specified outline color
+    void DrawOutline(Rect position, string text, GUIStyle style, Color outColor, Color inColor, int outlineSize)
+    {
+        style.normal.textColor = outColor;
+
+        for(int z = 1; z <= outlineSize+1; z++)
+        {
+            GUI.Label(new Rect(position.x - z,position.y,position.width,position.height), text, style);
+            GUI.Label(new Rect(position.x + z, position.y, position.width, position.height), text, style);
+            GUI.Label(new Rect(position.x, position.y - z, position.width, position.height), text, style);
+            GUI.Label(new Rect(position.x, position.y + z, position.width, position.height), text, style);
+        }
+
+        style.normal.textColor = inColor;
+        GUI.Label(position, text, style);
     }
 }
---
+++ HoP/Assets/Scripts/FadeOut.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOut : MonoBehaviour {
+
+    Renderer rend;
+    BoxCollider2D[] colliders;
+    bool unactive;
+    bool touched;
+    float disTimer;
+    float standTimer;
+
+    public float transparency = .3f;
+    public float timeDisabled = 2.0f;
+    public float timeOnPlatform = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        colliders = GetComponents<BoxCollider2D>();
+        unactive = false;
+        touched = false;
+        disTimer = timeDisabled;
+        standTimer = timeOnPlatform;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(touched && !unactive)
+        {
+            standTimer -= Time.deltaTime;
+            if (standTimer < 0)
+            {
+                rend.material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, transparency);
+                unactive = true;
+                touched = false;
+                colliders[0].enabled = false;
+                colliders[1].enabled = false;
+            }
+        }
+
+        if(unactive)
+        {
+            disTimer -= Time.deltaTime;
+
+            if (disTimer < 0)
+            {
+                rend.material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, 1.0f);
+                unactive = false;
+                standTimer = timeOnPlatform;
+                disTimer = timeDisabled;
+                colliders[0].enabled = true;
+                colliders[1].enabled = true;
+            }
+        }
+        
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!touched && !unactive)
+        {
+            touched = true;
+        }
+    }
+} 
