ded3f580f499faed9351ce8a4887c0c466217841
jokthefoo <alexkofford@gmail.com>
2017-03-08 18:01:29 -0700

    Added pause menu

---
+++ HoP/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerScript : MonoBehaviour
+{
+
+    public Canvas UI;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePauseMenu();
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ResetGame()
+    {
+        int scene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+
+    public void EnteredName()
+    {
+        string input;
+        input = UI.GetComponentInChildren<InputField>().text;
+        UI.GetComponentInChildren<InputField>().enabled = false;
+        UI.GetComponentInChildren<InputField>().GetComponent<Image>().enabled = false;
+        foreach ( Text t in UI.GetComponentsInChildren<Text>())
+        {
+            if(t.name == "Title")
+            {
+                t.text = "The Hardships of " + input;
+            }
+            if(t.name == "InputText")
+            {
+                t.enabled = false;
+            }
+        }
+    }
+
+    public void TogglePauseMenu()
+    {
+        if (UI.GetComponent<Canvas>().enabled)
+        {
+            UI.GetComponent<Canvas>().enabled = false;
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            UI.GetComponent<Canvas>().enabled = true;
+            Time.timeScale = 0f;
+        }
+    }
+
+}
---
+++ HoP/Assets/Scripts/SpriteRandomizer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRandomizer : MonoBehaviour
+{
+
+    public Sprite[] sprites;
+
+    // Use this for initialization
+    void Start()
+    {
+        foreach (SpriteRenderer s in GetComponentsInChildren<SpriteRenderer>())
+        {
+            int i = Random.Range(0, sprites.Length);
+            s.sprite = sprites[i];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
