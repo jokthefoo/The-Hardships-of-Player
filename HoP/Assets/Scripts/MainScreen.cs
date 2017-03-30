1c3aea6850ddcb7453e62d168a1a74b64efe9e03
jokthefoo <alexkofford@gmail.com>
2017-03-29 18:51:17 -0600

    Created start scene, added color picker.

---
+++ HoP/Assets/Scripts/MainScreen.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainScreen : MonoBehaviour {
+
+    public InputField input;
+    public Canvas nameCanvas;
+    public Canvas colorCanvas;
+    public ColorPicker picker;
+    string playerName;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        picker.CurrentColor = Color.green;
+        nameCanvas.gameObject.SetActive(true);
+        colorCanvas.gameObject.SetActive(false);
+        input.ActivateInputField();
+        playerName = "";
+        picker.CurrentColor = Color.green;
+
+        picker.onValueChanged.AddListener(color =>
+        {
+            player.GetComponent<SpriteColor>().ChangeSprite(color);
+        });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void NameEntered(string name)
+    {
+        playerName = name;
+        loadTitleColorSelect();
+    }
+
+    public void NameEntered()
+    {
+        foreach (Text t in input.GetComponentsInChildren<Text>())
+        {
+            if(t.gameObject.name == "Text")
+            {
+                playerName = t.text;
+                loadTitleColorSelect();
+            }
+        }
+    }
+
+    void loadTitleColorSelect()
+    {
+        nameCanvas.gameObject.SetActive(false);
+        foreach( Text t in colorCanvas.GetComponentsInChildren<Text>())
+        {
+            if(t.gameObject.name == "titleText")
+            {
+                t.text = t.text + playerName;
+            }
+        }
+        colorCanvas.gameObject.SetActive(true);
+    }
+}
--- HoP/Assets/Scripts/SpriteColor.cs
+++ HoP/Assets/Scripts/SpriteColor.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class SpriteColor : MonoBehaviour {
 
     Color baseColor = Color.green;
+    public Sprite BaseSprite;
 	// Use this for initialization
-	void Start () {
-       
+	void Start ()
+    {
+        InitSprite();
 	}
 
-    void ChangeSprite()
+    void InitSprite()
     {
-        Sprite temp = GetComponent<SpriteRenderer>().sprite;
+        Sprite temp = BaseSprite;
         Texture2D tempText = new Texture2D(temp.texture.width, temp.texture.height);
-
+        Color color = Color.green;
         Color c;
 
         for (int y = 0; y < tempText.height; y++)
@@ -24,66 +26,11 @@ public class SpriteColor : MonoBehaviour {
                 c = temp.texture.GetPixel(x, y);
                 if (c == baseColor)
                 {
-                    tempText.SetPixel(x, y, Color.red);
+                    tempText.SetPixel(x, y, color);
                 }
-                else if (c.g < .5 && c.r < .5 && c.b > .5)
+                else if (c.g >= .5 && c.r < .5 && c.b < .5)
                 {
-                    tempText.SetPixel(x, y, new Color(c.b, c.g, c.r));
-                }
-                else if(c.g > .5 && c.r < .5 && c.b < .5)
-                {
-                    tempText.SetPixel(x, y, new Color(c.g, c.r, c.b));
-                }else if(c == Color.white)
-                {
-                    if( x > 73 && x < 130 && y > 100 && y < 125)
-                    {
-                        tempText.SetPixel(x, y, Color.white);
-                    }
-                    else
-                    {
-                        tempText.SetPixel(x, y, new Color(0, 0, 0, 0));
-                    }
-                }
-                else
-                {
-                    tempText.SetPixel(x, y, c);
-                }
-
-            }
-        }
-        tempText.Apply();
-
-        SpriteRenderer sr = GetComponent<SpriteRenderer>();
-        print(sr.sprite.pivot);
-        sr.sprite = Sprite.Create(tempText, sr.sprite.textureRect, new Vector2(0,0));
-        sr.sprite.name = "tempname";
-        sr.material.mainTexture = tempText;
-        baseColor = Color.red;
-    }
-
-    void ChangeSpriteBlue()
-    {
-        Sprite temp = GetComponent<SpriteRenderer>().sprite;
-        Texture2D tempText = new Texture2D(temp.texture.width, temp.texture.height);
-
-        Color c;
-
-        for (int y = 0; y < tempText.height; y++)
-        {
-            for (int x = 0; x < tempText.width; x++)
-            {
-                c = temp.texture.GetPixel(x, y);
-                if (c == baseColor)
-                {
-                    tempText.SetPixel(x, y, Color.blue);
-                }
-                else if (c.g < .5 && c.r > .5 && c.b < .5)
-                {
-                    tempText.SetPixel(x, y, new Color(c.b, c.g, c.r));
-                }
-                else if (c.g > .5 && c.r < .5 && c.b < .5)
-                {
-                    tempText.SetPixel(x, y, new Color(c.r, c.b, c.g));
+                    tempText.SetPixel(x, y, new Color(color.r / 2, color.g / 2, color.b / 2));
                 }
                 else if (c == Color.white)
                 {
@@ -106,18 +53,15 @@ public class SpriteColor : MonoBehaviour {
         tempText.Apply();
 
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
-        print(sr.sprite.pivot);
         sr.sprite = Sprite.Create(tempText, sr.sprite.textureRect, new Vector2(0, 0));
         sr.sprite.name = "tempname";
         sr.material.mainTexture = tempText;
-        baseColor = Color.blue;
     }
 
-    void ChangeSpriteGreen()
+    public void ChangeSprite(Color color)
     {
-        Sprite temp = GetComponent<SpriteRenderer>().sprite;
+        Sprite temp = BaseSprite;
         Texture2D tempText = new Texture2D(temp.texture.width, temp.texture.height);
-
         Color c;
 
         for (int y = 0; y < tempText.height; y++)
@@ -127,15 +71,11 @@ public class SpriteColor : MonoBehaviour {
                 c = temp.texture.GetPixel(x, y);
                 if (c == baseColor)
                 {
-                    tempText.SetPixel(x, y, Color.green);
+                    tempText.SetPixel(x, y, color);
                 }
-                else if (c.g < .5 && c.r > .5 && c.b < .5)
+                else if (c.g >= .5 && c.r < .5 && c.b < .5)
                 {
-                    tempText.SetPixel(x, y, new Color(c.g, c.r, c.b));
-                }
-                else if (c.g < .5 && c.r < .5 && c.b > .5)
-                {
-                    tempText.SetPixel(x, y, new Color(c.r, c.b, c.g));
+                    tempText.SetPixel(x, y, new Color(color.r / 2, color.g / 2, color.b / 2));
                 }
                 else if (c == Color.white)
                 {
@@ -155,31 +95,28 @@ public class SpriteColor : MonoBehaviour {
 
             }
         }
+
         tempText.Apply();
 
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
-        print(sr.sprite.pivot);
+        sr.sprite = BaseSprite;
         sr.sprite = Sprite.Create(tempText, sr.sprite.textureRect, new Vector2(0, 0));
         sr.sprite.name = "tempname";
         sr.material.mainTexture = tempText;
-        baseColor = Color.green;
     }
 
     // Update is called once per frame
     void Update () {
-		if(Input.GetKeyDown(KeyCode.R))
-        {
-            ChangeSprite();
-        }
+    }
+}
 
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            ChangeSpriteBlue();
-        }
+class Points
+{
+    public int x { get; set; }
+    public int y { get; set; }
 
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            ChangeSpriteGreen();
-        }
+    public override string ToString()
+    {
+        return string.Format("({0}, {1})", x, y);
     }
 }
