c1a338c38304b3468e60d000e7caba3c74ec6195
jokthefoo <alexkofford@gmail.com>
2017-04-19 20:32:22 -0600

    Added stats

--- HoP/Assets/Scripts/Vars.cs
+++ HoP/Assets/Scripts/Vars.cs
@@ -3,10 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Vars{
+
+public static class Vars
+{
 
     public static List<Sprite> sprites = new List<Sprite>();
     public static Texture2D text;
     public static String Name = "";
+    public static int numJumps = 0;
+    public static int momJumps = 0;
+    public static int dadJumps = 0;
+    public static int badJumps = 0;
+    public static int goodJumps = 0;
+    public static int friend1Jumps = 0;
+    public static int friend2Jumps = 0;
+    public static int brotherJumps = 0;
 
 }
+
